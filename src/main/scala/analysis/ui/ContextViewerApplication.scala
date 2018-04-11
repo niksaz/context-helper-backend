a3c936e153929e7fa56be1a3377d84dcbea82f57
niksaz <nikitasazanovich@gmail.com>
2018-04-11 11:04:07 +0300

    Same colors for the same installID.

--- src/main/scala/analysis/ui/ContextViewerApplication.scala
+++ src/main/scala/analysis/ui/ContextViewerApplication.scala
@@ -8,20 +8,38 @@ import scala.collection.mutable
 import scala.io.Source
 import scala.swing._
 import scala.swing.event.{EditDone, SelectionChanged}
+import scala.util.Random
 
 object ContextViewerApplication extends SimpleSwingApplication {
   private val appDimensions = Toolkit.getDefaultToolkit.getScreenSize
   private val testInstallId = "a590add0-f56f-4053-8b65-b27fe89c239d"
+  private val random = new Random(0)
+  private val testInstallToColor = mutable.Map[String, Color]()
 
   def top: MainFrame = new MainFrame {
     title = "Context Viewer"
-    object fileList extends ListView[String] {
+
+    object fileList extends ListView[(String, String)] {
       listData = listNonTestContexts()
+      renderer = (list: ListView[_ <: (String, String)], isSelected: Boolean, focused: Boolean, a: (String, String), index: Int) => {
+        val data = list.listData(index)
+        val label = new Label
+        label.text = data._1
+        label.opaque = true
+        if (!testInstallToColor.contains(data._2)) {
+          testInstallToColor.put(data._2, new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256)))
+        }
+        label.background = testInstallToColor(data._2)
+        label
+      }
     }
+
     val sessionList = new ListView[String]
+
     object contextNumber extends TextField {
       maximumSize = new Dimension(maximumSize.width, preferredSize.height)
     }
+
     val installIdLabel = new Label
     installIdLabel.opaque = true
     val sessionIdLabel = new Label
@@ -57,12 +75,7 @@ object ContextViewerApplication extends SimpleSwingApplication {
     def updateView(): Unit = {
       val (installId, sessionId, position, sourceCode) = readContext(contextNumber.text)
       installIdLabel.text = installId
-      installIdLabel.background =
-        if (installId.equals(testInstallId)) {
-          Color.red
-        } else {
-          Color.gray
-        }
+      installIdLabel.background = testInstallToColor(installId)
       fileTextPane.text = ""
       sessionList.listData = findRelevant(sessionId)
       sessionIdLabel.text = sessionId
@@ -71,6 +84,7 @@ object ContextViewerApplication extends SimpleSwingApplication {
       textPane.caret.position = position.toInt
       textPane.requestFocus()
     }
+
     listenTo(contextNumber, sessionList.selection, fileList.selection)
 
     reactions += {
@@ -85,20 +99,21 @@ object ContextViewerApplication extends SimpleSwingApplication {
           textPane.requestFocus()
         }
       case SelectionChanged(`fileList`) =>
-        contextNumber.text = fileList.selection.items(0)
+        contextNumber.text = fileList.selection.items(0)._1
         updateView()
       case EditDone(`contextNumber`) =>
         updateView()
     }
   }
 
-  private def listNonTestContexts(): Seq[String] = {
+  private def listNonTestContexts(): Seq[(String, String)] = {
     val contextsPath = new File("/Users/niksaz/Downloads/data/contexts")
-    contextsPath.listFiles().filter { file =>
+    contextsPath.listFiles().map { file =>
       val scanner = new Scanner(file)
       val installId = scanner.nextLine()
-      !installId.equals(testInstallId)
-    }.map(_.getName).sortBy(_.toInt)
+      scanner.close()
+      (file.getName, installId)
+    }.filterNot(_._2 == testInstallId).sortBy(_._1.toInt)
   }
 
   private def readContext(contextName: String): (String, String, String, String) = {
@@ -109,6 +124,7 @@ object ContextViewerApplication extends SimpleSwingApplication {
     while (scanner.hasNext) {
       lines.append(scanner.nextLine())
     }
+    scanner.close()
     (lines.head, lines(1), lines(2), lines.drop(3).mkString("\n"))
   }
 
@@ -117,18 +133,21 @@ object ContextViewerApplication extends SimpleSwingApplication {
     val clicks = clicksPath.listFiles().filter { file =>
       val scanner = new Scanner(file)
       val clickSessionId = scanner.nextLine()
+      scanner.close()
       clickSessionId.equals(sessionId)
     }.map("clicks/" + _.getName)
     val questionsPath = new File("/Users/niksaz/Downloads/data/questions")
     val questions = questionsPath.listFiles().filter { file =>
       val scanner = new Scanner(file)
       val questionSessionId = scanner.nextLine()
+      scanner.close()
       questionSessionId.equals(sessionId)
     }.map("questions/" + _.getName)
     val helpfulsPath = new File("/Users/niksaz/Downloads/data/helpful")
     val helpfuls = helpfulsPath.listFiles().filter { file =>
       val scanner = new Scanner(file)
       val helpfulSessionId = scanner.nextLine()
+      scanner.close()
       helpfulSessionId.equals(sessionId)
     }.map("helpful/" + _.getName)
     Seq(clicks, questions, helpfuls).flatten
